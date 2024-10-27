867153b036877769b9a6cd5156ee52a3cad21ef9
Jer403 <jeremiwtfxd@gmail.com>
2024-10-26 22:18:42 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const {
-    PORT = 3000,
-    SALT_ROUNDS = 10,
-    SECRET_JWT_KEY = "s6yz4BESuNzMJ98ZK1v4uqcffBZiKh2bVzpCQau7pM8QlgbFjtbRJvbx8WWpzerGBsLAUHf1xM4fErhPb1cvk1d962bY5qGY"
-} = process.env
